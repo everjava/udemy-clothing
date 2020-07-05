c0c7cdbc802833663762b0c33960c6cf0290802a
everjava <everjava@yahoo.com.br>
2020-07-05 14:18:50 -0300

    Lesson 74. Added Header component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { hot } from "react-hot-loader";
 import { Homepage } from "./pages/homepage.component";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import ShopPage from "./components/shop/shop.component";
- 
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-     
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />

36f1b3585ea5de023d7e75dab1ff0119ea5934ac
everjava <everjava@yahoo.com.br>
2020-07-11 16:31:59 -0300

    Lesson 128. Added checkout item, remove items from cart and checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   //se nao existe o item no carItems, add o primeiro
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+
+ 
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }//retorna o array inicial mais o qtty atualizado
+      : cartItem
+  );
+};

852abc595b3597056b93993d9b5c9b6b7b644175
everjava <everjava@yahoo.com.br>
2020-07-12 14:47:47 -0300

    Lesson 130. Added redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from 'redux-logger'
 import rootReducer from "./root-reducer";
+import { persistStore } from 'redux-persist';
  
  const middlewares = [ logger];//se precisar add+, só add neste array
 
- const store = createStore(rootReducer, applyMiddleware(...middlewares))
+ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-
- export default store
+ export const persistor = persistStore(store);

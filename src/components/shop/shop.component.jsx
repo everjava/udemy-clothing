4ae1c4b0744e949325e88fc1f8cc0b7279367ed8
everjava <everjava@yahoo.com.br>
2020-07-12 17:06:20 -0300

    Lesson 133. Added CollectionOverview no lugar de shop-component

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
-//import data from "./data.json";
-import CollectionPreview from "../collection-preview/collection-preview.component";
-import { connect } from "react-redux";
-import { selectCollections } from "../../redux/shop/shop.selectors";
-import { createStructuredSelector } from "reselect";
+ 
+import CollectionsOverview from "../collections-overview/collections-overview.component";
+ 
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ collections }) => {  
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+     <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+ 
+export default  ShopPage ;
